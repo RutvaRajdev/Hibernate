6107476810654f4561f21c4856bec56f4880a43e
Rutva_Rajdev <rutva.rajdev@gmail.com>
2019-08-21 11:43:00 -0400

    Created one more Object and table. Applied Hibernate CRUD features

---
+++ src/com/handson/hibernate/demo2/QueryEmployee.java
@@ -0,0 +1,38 @@
+package com.handson.hibernate.demo2;
+
+import com.handson.hibernate.demo2.entity.Employee;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import java.util.List;
+
+public class QueryEmployee {
+    public static void main(String[] args) {
+        SessionFactory sessionFactory = new Configuration()
+                .configure()
+                .addAnnotatedClass(Employee.class)
+                .buildSessionFactory();
+
+        Session session = sessionFactory.getCurrentSession();
+
+        try {
+            System.out.println("Querying Armedia employees");
+
+            session.beginTransaction();
+            List<Employee> employees = session.createQuery("from Employee where company='Armedia'").list();
+
+            for(Employee employee : employees)
+                System.out.println(employee);
+
+            session.getTransaction().commit();
+
+            System.out.println("Done!");
+
+
+        }
+        finally {
+            sessionFactory.close();
+        }
+    }
+}
---
+++ src/com/handson/hibernate/demo2/ReadEmployee.java
@@ -0,0 +1,40 @@
+package com.handson.hibernate.demo2;
+
+import com.handson.hibernate.demo2.entity.Employee;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class ReadEmployee {
+
+    public static void main(String[] args) {
+        SessionFactory factory = new Configuration()
+                .configure()
+                .addAnnotatedClass(Employee.class)
+                .buildSessionFactory();
+
+        Session session = factory.getCurrentSession();
+
+        try {
+
+            System.out.println("Reading Employee with id 1");
+
+            session.beginTransaction();
+
+            Employee employee = session.get(Employee.class, 1);
+
+            System.out.println("Employee: "+employee);
+
+            session.getTransaction().commit();
+
+            System.out.println("Done!");
+
+        }
+        finally {
+
+            factory.close();
+
+        }
+    }
+
+}
