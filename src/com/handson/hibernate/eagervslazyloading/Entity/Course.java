4aac1ce584a3b4be8bc36c6961264ee06a488e16
Rutva_Rajdev <rutva.rajdev@gmail.com>
2019-08-22 13:58:53 -0400

    Tested eager loading using debug

---
+++ src/com/handson/hibernate/eagervslazyloading/Entity/Course.java
@@ -0,0 +1,65 @@
+package com.handson.hibernate.eagervslazyloading.Entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "course")
+public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "title")
+    private String title;
+
+    public Course() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                '}';
+    }
+
+    public Course(String title) {
+        this.title = title;
+    }
+
+
+    public Instructor getInstructor() {
+        return instructor;
+    }
+
+    public void setInstructor(Instructor instructor_onetomany) {
+        this.instructor = instructor_onetomany;
+    }
+
+    @ManyToOne(cascade = {
+            CascadeType.DETACH,
+            CascadeType.MERGE,
+            CascadeType.PERSIST,
+            CascadeType.REFRESH
+    })
+    @JoinColumn(name = "instructor_id")
+    private Instructor instructor;
+}
