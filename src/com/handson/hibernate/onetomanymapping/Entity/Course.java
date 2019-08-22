558868e99f9b3d3b81ee4835c1a632ac7c9eee71
Rutva_Rajdev <rutva.rajdev@gmail.com>
2019-08-22 11:35:41 -0400

    Created and annotated course class

---
+++ src/com/handson/hibernate/onetomanymapping/Entity/Course.java
@@ -0,0 +1,57 @@
+package com.handson.hibernate.onetomanymapping.Entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "course")
+public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "title")
+    private String title;
+
+    public Course() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", instructor=" + instructor +
+                '}';
+    }
+
+    public Course(String title) {
+        this.title = title;
+    }
+
+    @ManyToOne(cascade = {
+            CascadeType.DETACH,
+            CascadeType.MERGE,
+            CascadeType.PERSIST,
+            CascadeType.REFRESH
+    })
+    @JoinColumn(name = "instructor_id")
+    private Instructor instructor;
+}
