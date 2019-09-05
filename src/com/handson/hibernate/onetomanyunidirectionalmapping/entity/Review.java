903f9ec036ac83e813c06c0eae97a6184351838b
Rutva_Rajdev <rutva.rajdev@gmail.com>
2019-09-05 09:24:54 -0400

    created new package for onetomany uni directional mapping

---
+++ src/com/handson/hibernate/onetomanyunidirectionalmapping/entity/Review.java
@@ -0,0 +1,48 @@
+package com.handson.hibernate.onetomanyunidirectionalmapping.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "review")
+public class Review {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Override
+    public String toString() {
+        return "Review{" +
+                "id=" + id +
+                ", comment='" + comment + '\'' +
+                '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public Review(String comment) {
+        this.comment = comment;
+    }
+
+    public Review() {
+    }
+
+    @Column(name = "comment")
+    private String comment;
+
+}
